61888db018c93162902e7ca200b8e6de366fa171
RodrigoIDS <rodrigo.escutia@ids.com.mx>
2022-06-24 08:55:02 -0500

    Proyecto funcional

--- Bdd_Bau_Checkin/src/test/java/karate-config.js
+++
@@ -1,7 +0,0 @@
-function fn() {
-	karate.configure('ssl', true);
-	//karate.configure('connectTimeout', 15000);
-	//karate.configure('readTimeout', 16000);
-	return baseUrl ='https://nrt.amlab7.com/';
-	//return baseUrl = 'https://am-ecommerce-dev.apigee.net/'
-}
